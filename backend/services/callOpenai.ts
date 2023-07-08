4f936ba5e658e5784c42bfd01334abca61aa67fa
xamdel <xamdel@protonmail.com>
2023-07-08 13:37:47 -0700

    create function for calling OpenAI API

---
+++ backend/services/callOpenai.ts
@@ -0,0 +1,15 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+// Export flexible function to be called with prompt templates and specified model
+export async function generateCompletion(prompt: string, model: string) {
+  const completion = await openai.createChatCompletion({
+    model: model,
+    messages: prompt,
+  });
+  return completion.data.choices[0].message;
+}

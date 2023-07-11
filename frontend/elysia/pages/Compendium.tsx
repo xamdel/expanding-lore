161b52b9b7f70c0dcb4e022d1fdcd2510703c19d
xamdel <xamdel@protonmail.com>
2023-07-11 09:40:23 -0700

    add Compendium homepage

---
+++ frontend/elysia/pages/Compendium.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import styles from '../styles/Compendium.module.css'
+
+export default function Compendium() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.cardContainer}>
+        <Link href="/Compendium/Storylines">
+          <div className={styles.card}>Storylines</div>
+        </Link>
+        <Link href="/Compendium/Locations">
+          <div className={styles.card}>Locations</div>
+        </Link>
+        <Link href="/Compendium/Factions">
+          <div className={styles.card}>Factions</div>
+        </Link>
+        <Link href="/Compendium/Characters">
+          <div className={styles.card}>Characters</div>
+        </Link>
+        <Link href="/Compendium/Other">
+          <div className={styles.card}>Other</div>
+        </Link>
+      </div>
+    </div>
+  )
+}

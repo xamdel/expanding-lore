caef0cbd96a4a0d147eedadabffc5fd23502583b
xamdel <xamdel@protonmail.com>
2023-07-13 16:15:24 -0700

    add analytics for webpage

--- frontend/elysia/pages/_app.tsx
+++ frontend/elysia/pages/_app.tsx
@@ -1,12 +1,14 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics/>
     </Layout>
   )
 }
